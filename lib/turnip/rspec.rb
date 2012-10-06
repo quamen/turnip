7545aeefcecc800ff997f810ec6997f57ab6fa13
Gareth Townsend <gareth.townsend@me.com>
2012-10-06 11:52:41 +1000

    Only print the scenario description in rspec output.
    * Remove the '->' concatinated step descriptions from the rspec output.

--- lib/turnip/rspec.rb
+++ lib/turnip/rspec.rb
@@ -65,7 +65,7 @@ module Turnip
             end
             feature.scenarios.each do |scenario|
               describe scenario.name, scenario.metadata_hash do
-                it scenario.steps.map(&:description).join(' -> ') do
+                it '' do
                   scenario.steps.each do |step|
                     run_step(feature_file, step)
                   end
